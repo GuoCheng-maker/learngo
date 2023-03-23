6b059ea896d06e3b3ee4355c4ffcc329cdc090cb
guocheng <cheng.guo@payby.com>
2023-03-23 11:19:21 +0800

    fix

--- code/video25/channel_demo4/main.go
+++ code/video25/channel_demo4/main.go
@@ -10,10 +10,10 @@ var wg sync.WaitGroup
 
 func Provider(data chan<- int, name string) {
 	for i := 0; i < 100; i++ {
-		//保证传入i的操作和print操作"原子运行"
 		select {
 		case data <- i:
 			fmt.Println(name, "生产了:", i)
+		default:
 		}
 	}
 	wg.Done()
@@ -21,17 +21,20 @@ func Provider(data chan<- int, name string) {
 
 func Consumer(data <-chan int, name string) {
 	for {
-		tmp, ok := <-data
-		if !ok {
-			break
+		select {
+		case tmp, ok := <-data:
+			if !ok {
+				wg.Done()
+				return
+			}
+			fmt.Println(name, "读取了", tmp)
+		default:
+			fmt.Println("============================================================================")
 		}
-		fmt.Println(name, "读取了", tmp)
 	}
-	wg.Done()
 }
 
 func main() {
-	//定义缓冲区大小
 	data := make(chan int, 10)
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
