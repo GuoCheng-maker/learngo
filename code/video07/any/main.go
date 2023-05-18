e5ae9764e598797919e6d19115f44db936a5e690
guocheng <cheng.guo@payby.com>
2023-05-18 11:58:51 +0800

    add any

--- code/video07/any/main.go
+++ code/video07/any/main.go
@@ -36,6 +36,22 @@ func reverseAny[T any](s []T) []T {
 	return r
 }
 
+func MapKeys[Key comparable, Val any](m map[Key]Val) []Key {
+	s := make([]Key, 0, len(m))
+	for k := range m {
+		s = append(s, k)
+	}
+	return s
+}
+
+func MapKeysInt[Key int, Val any](m map[Key]Val) []Key {
+	n := make([]Key, 0, len(m))
+	for k := range m {
+		n = append(n, k)
+	}
+	return n
+}
+
 func main() {
 	reverse([]int{1, 2, 3, 4, 5})
 	reverseIF([]int{1, 2, 3, 4, 5})
@@ -43,5 +59,10 @@ func main() {
 	// reverseIF([]float32{1, 2, 3, 4, 5})  // 泛型中只对int和float64类型做了处理，所以这里会报错，也可以使用any类型
 	reverseAny([]string{"1", "2", "3", "4", "5"})
 	reverseAny([]float32{1.1, 2.2, 3.3, 4.4, 5.5})
+	m := map[int]interface{}{1: true, 2: "2", 3: "3.00"}
+	ret := MapKeys(m)
+	fmt.Println(ret)
 
+	ret2 := MapKeysInt(m)
+	fmt.Println(ret2)
 }
